8ef171d5d1a29f6808d0bc961fa6f6b10823d8a4
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-03-03 17:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const yourArray = [
-	[{name:"hello", age:20, female:true}],
-	[{name:"ram", age:20, female:false}],
-	[{name:"shyam", age:20, female:true}],
-	[{name:"dham", age:20, female:true}],
-	[{name:"kam", age:20, female:true}]
-];
-console.log(yourArray);
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray);
